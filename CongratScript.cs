aa4541dfaf8a6e52be6358850d1f08717ccae884
tutbora <tutbora@gmail.com>
2021-11-08 19:10:07 +0300

    Debug logic in the script

---
+++ CongratScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CongratScript : MonoBehaviour {
+    public TextMesh Text;
+    public ParticleSystem SparksParticles;
+    private List<string> TextToDisplay = new List<string>(3);
+    private float TimeToNextText;
+    private int CurrentText;
+
+    // Start is called before the first frame update
+    void Start() {
+        TimeToNextText = 0.0f;
+        CurrentText = 0;
+        TextToDisplay.Add("Hello!");
+        TextToDisplay.Add("Congratulation");
+        TextToDisplay.Add("All Errors Fixed");
+        SparksParticles.Play();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        TimeToNextText += Time.deltaTime;
+
+        if (TimeToNextText > 1.5f)
+        {
+            TimeToNextText = 0.0f;
+
+            if (CurrentText >= TextToDisplay.Count)
+            {
+                CurrentText = 0;
+            }
+            Text.text = TextToDisplay[CurrentText];
+            CurrentText++;
+        }
+    }
+}
