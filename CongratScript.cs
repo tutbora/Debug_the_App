cd73d762d994fe8920379ba12dc92088da5e4d75
tutbora <tutbora@gmail.com>
2021-11-08 21:16:17 +0300

    Debug logic in the script + from quiz

--- CongratScript.cs
+++ CongratScript.cs
@@ -8,15 +8,21 @@ public class CongratScript : MonoBehaviour {
     private float TimeToNextText;
     private int CurrentText;
 
-    // Start is called before the first frame update
-    void Start() {
-        TimeToNextText = 0.0f;
-        CurrentText = 0;
-        TextToDisplay.Add("Hello!");
-        TextToDisplay.Add("Congratulation");
-        TextToDisplay.Add("All Errors Fixed");
-        SparksParticles.Play();
+    string playerName = "Frank";
+    void Start()
+    {
+        Debug.Log("Hello: " + gameObject.name + playerName);
     }
+
+    // Start is called before the first frame update
+//    void Start() {
+//        TimeToNextText = 0.0f;
+//        CurrentText = 0;
+//        TextToDisplay.Add("Hello!");
+//        TextToDisplay.Add("Congratulation");
+//        TextToDisplay.Add("All Errors Fixed");
+//        SparksParticles.Play();
+//    }
 
     // Update is called once per frame
     void Update() {
@@ -33,5 +39,24 @@ public class CongratScript : MonoBehaviour {
             Text.text = TextToDisplay[CurrentText];
             CurrentText++;
         }
+        if (true && !false) { Debug.Log("Success"); }
+        if (true || false) { Debug.Log("Success"); }
+        if (true && !true) { Debug.Log("Success"); }
+        if (false || !true) { Debug.Log("Success"); }
+
+        string[] strings = new string[3];
+
+        strings[0] = "First string";
+        strings[1] = "Second string";
+        strings[2] = "Third string";
+        foreach (string item in strings)
+        {
+            print (item);
+        }
+
+        List<GameObject> objectList = new List<GameObject>();
+        GameObject[] objectArray = new GameObject[];
+        Dictionary<string, GameObject> objectDict = new Dictionary<string, GameObject>();
+        bool isTrue = true;
     }
 }
